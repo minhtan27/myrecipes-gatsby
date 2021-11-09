c7e3044bae015055c755b4472c7afd0f75263197
Tan Minh <tmt.study.27@gmail.com>
2021-11-09 10:20:33 +0700

    finalize blog page

--- src/pages/blogs/index.js
+++ src/pages/blogs/index.js
@@ -1,14 +1,58 @@
 import React from "react";
 import Layout from "../../component/Layout";
 import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { Link } from "gatsby";
 
 const Blogs = ({ data }) => {
-  console.log(data);
+  const {
+    allContentfulMrRecipesBlog: { nodes },
+  } = data;
+  console.log(nodes);
 
   return (
     <Layout>
       <main className="page">
-        <h2>Blog Post</h2>
+        <section className="blog-hero">
+          <GatsbyImage
+            image={getImage(nodes[3].image[0])}
+            alt={nodes[3].title}
+            className="b-h-img"
+          ></GatsbyImage>
+          <div className="b-h-text">
+            <h2>Welcome to our blog post</h2>
+          </div>
+        </section>
+        <section className="blog-container">
+          {nodes.map((item) => {
+            const {
+              id,
+              title,
+              subtitle: { subtitle },
+            } = item;
+            const image = getImage(item.image[0]);
+            return (
+              <div className="b-c-item" key={id}>
+                <Link to={`/blogs/${id}`}>
+                  <GatsbyImage
+                    image={image}
+                    alt={title}
+                    className="b-c-i-img"
+                  ></GatsbyImage>
+                </Link>
+                <div className="b-c-i-text">
+                  <h3>
+                    <Link to={`/blogs/${id}`} className="link">
+                      {title}
+                    </Link>
+                  </h3>
+
+                  <p>{subtitle}</p>
+                </div>
+              </div>
+            );
+          })}
+        </section>
       </main>
     </Layout>
   );
@@ -23,6 +67,9 @@ export const query = graphql`
         id
         title
         date
+        image {
+          gatsbyImageData(placeholder: TRACED_SVG, layout: FULL_WIDTH)
+        }
         content {
           raw
         }
